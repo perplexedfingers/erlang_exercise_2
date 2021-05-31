75a8a2bcbf043cd9a4a126364288924941a72002
Ray Zhu <14918698+perplexedfingers@users.noreply.github.com>
2021-05-31 04:51:18 +0000

    Correct `counter` usage

--- apps/erlang_exercise/src/the_server.erl
+++ apps/erlang_exercise/src/the_server.erl
@@ -7,18 +7,25 @@
 -export([init/1, handle_call/3, handle_cast/2]).
 
 start_link() ->
-    gen_event:add_handler(info_man, terminal_logger, []),
-    gen_server:start_link({local, server}, the_server, [], []).
+    gen_server:start_link({local, server}, the_server, []).
 
-init(_Args) ->
-    {ok, 0}.  % Let initial value be adjustable
+init([InitVal]) when is_integer(InitVal) ->
+    gen_event:add_handler(info_man, terminal_logger, []),
+    CounterRef = counters:new(1, [atomics]),
+    counters:put(CounterRef, 1, InitVal),
+    {ok, CounterRef};
+init([]) ->
+    gen_event:add_handler(info_man, terminal_logger, []),
+    CounterRef = counters:new(1, [atomics]),
+    counters:put(CounterRef, 1, 0),
+    {ok, CounterRef}.
 
 terminate(shutdown, _State) ->
     ok.
 
 show_now() ->
-    gen_event:notify(info_man, "Query current value"),
-    gen_server:call(server, show_now).
+    %gen_event:notify(info_man, "Query current value"),
+    gen_server:call(server, show).
 
 go_up() ->
     gen_event:notify(info_man, io:format("The value does UP and become '~p'~n", [what])),
@@ -28,10 +35,11 @@ go_down() ->
     gen_event:notify(info_man, io:format("The value does DOWN and become '~p'~n", [what])),
     gen_server:call(server, down).
 
-handle_call(up, _From, Count) ->
-    {reply, ok, Count + 1};
-handle_call(show_now, _From, Count) ->
-    {reply, Count, Count}.
+handle_call(up, _From, CounterRef) ->
+    counters:add(CounterRef, 1, 1),
+    {reply, ok, CounterRef};
+handle_call(show, _From, CounterRef) ->
+    {reply, counters:get(CounterRef, 1), CounterRef}.
 
 % Reset the value
 % handle_cast({free, Ch}, Chs) ->
--- test/server_test.erl
+++ test/server_test.erl
@@ -2,8 +2,14 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-
 simple_test() ->
+    %CounterRef = counters:new(1, [atomics]),
+    %?assertEqual(0, counters:get(CounterRef, 1)),
+    %counters:put(CounterRef, 1, 3),
+    %?assertEqual(3, counters:get(CounterRef, 1)),
+    %counters:add(CounterRef, 1, 1),
+    %?assertEqual(4, counters:get(CounterRef, 1)).
+
     the_server:start_link(),
     ?assertEqual(0, the_server:show_now()),
     the_server:go_up(),
