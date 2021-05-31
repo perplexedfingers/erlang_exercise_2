c14c69d1f445239a392ac3bc8ecc6bfd4c25e842
Ray Zhu <14918698+perplexedfingers@users.noreply.github.com>
2021-05-31 05:35:00 +0000

    Correct usage for `gen_event` in test case

--- test/server_test.erl
+++ test/server_test.erl
@@ -2,15 +2,11 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
-simple_test() ->
-    %CounterRef = counters:new(1, [atomics]),
-    %?assertEqual(0, counters:get(CounterRef, 1)),
-    %counters:put(CounterRef, 1, 3),
-    %?assertEqual(3, counters:get(CounterRef, 1)),
-    %counters:add(CounterRef, 1, 1),
-    %?assertEqual(4, counters:get(CounterRef, 1)).
+-import(the_server, [start_link/1, show_now/0, go_up/0]).
 
-    the_server:start_link(),
-    ?assertEqual(0, the_server:show_now()),
-    the_server:go_up(),
-    ?assertEqual(1, the_server:show_now()).
+simple_test() ->
+    gen_event:start_link({local, info_man}),
+    start_link(0),
+    ?assertEqual(0, show_now()),
+    go_up(),
+    ?assertEqual(1, show_now()).
